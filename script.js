0b42d93224929363963e0ba00b7798a2b93fc837
kihBootCamp <kimberleyheuer@hotmail.com>
2020-12-13 15:58:20 -0600

    cleaning up

--- script.js
+++ script.js
@@ -15,69 +15,44 @@ $(document).ready(function () {
     var localDate = today.toLocaleDateString();
 
 
-    init_5_day_forecast_cards();
-
+    // when there is a click on the city list
     $("li").on("click", function (e) {
         var city = $(this).text();
         console.log("item click " + $(this).text())
 
-        get_current_weather(city);
+        get_forecast_weather(city);
     })
 
-    function get_current_weather(city) {
-
-        $.ajax({
-            url: weather_url + "&q=" + city,
-            method: "GET"
-        }).then(function (weatherData) {
-
-            console.log(weatherData);
-
-            console.log(weatherData.weather[0].main);  //icon setting
-            console.log(weatherData.main.temp);  //icon setting
-            console.log(weatherData.main.humidity);  //temp
-            console.log(weatherData.wind.speed);  //temp
-            console.log(weatherData.main.humidity);  //temp
-
-
-            //   $(".city").text(weatherData.name);
-
-            //   $(".wind").text(weatherData.wind.speed);
-
-            //   $(".humidity").text(weatherData.main.humidity);
-
-            //   $(".temp").text(weatherData.main.temp);
-
-            get_forecast_weather(city);
-        })
-
-    }
 
     function get_forecast_weather(city) {
         var lat;
         var lon;
+        var main;
+        var uv_index;
 
-        // also includes todays weather forecast
+        // this also includes todays weather forecast
         $.ajax({
             url: forecast_url + "&q=" + city,
             method: "GET"
         }).then(function (weatherData) {
 
-            //console.log(weatherData);
+            console.log(weatherData);
+            $(".main-card-title-custom").text(weatherData.city.name + " (" + localDate + ")");
 
-            console.log(weatherData.list[0].weather[0].main);  //icon setting
-            //console.log(weatherData.list[0].main.temp);
+            // the main is a one word description of the weather
+            // use this word to set the icon
+
+            main = weatherData.list[0].weather[0].main;
+            set_weather_icon(main, "#main-icon");
+
             $("#temp").text("Temperature: " + weatherData.list[0].main.temp + " F\xB0;");
             //console.log(weatherData.list[0].main.humidity);
             $("#humidity").text("Humidity: " + weatherData.list[0].main.humidity + " %");
             //console.log(weatherData.list[0].wind.speed);
             $("#wind").text("Wind Speed: " + weatherData.list[0].wind.speed + " MPH");
 
-
             lat = weatherData.city.coord.lat;
             lon = weatherData.city.coord.lon
-            console.log(lat);
-            console.log(lon);
 
             // fetch the uv index with the latitude and longitude info
             $.ajax({
@@ -87,26 +62,60 @@ $(document).ready(function () {
                 console.log(uvData);
 
                 $("#uv-index").text("UV Index: " + uvData.value);
+                $("#uv-index").css("background-color", "red");
             })
 
+            //uv_index = get_uv_index(lat, lon);
+            //console.log("back"  + uv_index);
+            //$("#uv-index").text("UV Index: " + uv_index);
 
+            // Loop through the next 5 days displaying temp and humidity for the 5-day forecast
 
-            //   $(".city").text(weatherData.name);
-            //   $(".wind").text(weatherData.wind.speed);
-            //   $(".humidity").text(weatherData.main.humidity);
-            //   $(".temp").text(weatherData.main.temp);
+            for (var i = 1; i < 6; i++) {
 
+                $("#card-temp" + i).text("Temp: " + weatherData.list[i].main.temp + " F\xB0;")
+                $("#card-humid" + i).text("Humidity: " + weatherData.list[i].main.humidity + " %");
 
+                set_weather_icon(weatherData.list[i].weather[0].main, "#card-icon" + i)
+            }
         })
+
+        // not sequential need call back not working
+        // see https://stackoverflow.com/questions/15847292/function-that-return-a-value-from-ajax-call-request
+        function get_uv_index(lat, lon) {
+            // fetch the uv index with the latitude and longitude info
+            $.ajax({
+                url: weather_uv_url + "&lat=" + lat + "&lon=" + lon,
+                method: "GET"
+            }).then(function (uvData) {
+                console.log("here!");
+                console.log(uvData.value);
+                return (uvData.value);
+
+                //$("#uv-index").text("UV Index: " + uvData.value);
+            })
+        }
+
+        function set_weather_icon(weather_type, idset) {
+
+            if (weather_type === "Rain")
+                $(idset).attr("src", "./Assets/storm.png");
+            else if (weather_type === "Clouds")
+                $(idset).attr("src", "./Assets/cloudy.png");
+            else if (weather_type === "Snow")
+                $(idset).attr("src", "./Assets/snowflake.png");
+            else if (weather_type === "Clear")
+                $(idset).attr("src", "./Assets/sun.png");
+        }
 
     }
 
     function init_5_day_forecast_cards() {
 
         // main card titles        
-        $(".main-card-title-custom").text("Arlington Heights (" + localDate + ")");
+        $(".main-card-title-custom").text("Select a city! (" + localDate + ")");
 
-        // add a day to today
+        // add a day to today for the next 5 days
         var nextDay = new Date();
         nextDay.setDate(nextDay.getDate() + 1);
         var nextLocalDate1 = nextDay.toLocaleDateString();
@@ -114,8 +123,6 @@ $(document).ready(function () {
         var nextDay2 = new Date();
         nextDay2.setDate(nextDay2.getDate() + 2);
         var nextLocalDate2 = nextDay2.toLocaleDateString();
-        console.log(nextDay2);
-        console.log(nextLocalDate2);
 
         var nextDay3 = new Date();
         nextDay3.setDate(nextDay3.getDate() + 3);
@@ -136,11 +143,7 @@ $(document).ready(function () {
         $("#card-day-5").text(nextLocalDate5);
     }
 
-
-
-
-
-
-
-
+    // initialize the weather cards
+    init_5_day_forecast_cards();
+    
 })    
